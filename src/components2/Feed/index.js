c76f4c7dfa40525deb69fded21f95dbccece9986
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-08-29 19:19:39 -0300

    learning about using lists with states

--- src/components2/Feed/index.js
+++ src/components2/Feed/index.js
@@ -4,18 +4,7 @@ class Feed extends Component {
     render() {
         return (
             <div>
-                {/* o map roda a lista e passa por objeto por objeto */}
-                {this.state.feed.map((item) => {
-                    return (
-                        // temos que passar essa key para o react não reclamar.
-                        // agora cada div tem um id unico
-                        <div key={item.id}>
-                            <h3>{item.username}</h3>
-                            <a>{item.curtidas} curtidas/ 
-                                {item.comentarios} comentarios</a>
-                        </div>
-                    )
-                })}
+                
             </div>
         );
     }
